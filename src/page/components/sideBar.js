7242bd4f6fc13fc6de9134e6130ae0792009cc4c
Sandip Thapa <sandip074bct036@ioepc.edu.np>
2021-11-16 19:44:32 +0545

    sidebar completed

--- src/page/components/sideBar.js
+++ src/page/components/sideBar.js
@@ -6,47 +6,59 @@ import { IoGridOutline } from "react-icons/io5";
 import { FiGithub } from "react-icons/fi";
 
 import "./sideBar.css";
+import React from "react";
 
 const SideBar = (props) => {
   console.log(props.name);
 
+  // let btn = document.querySelector("#btn") 
+  let sidebar = document.querySelector(".sidebar")
+  const buttonClick = ()=>{
+    console.log('hi')
+    sidebar.classList.toggle('active')
+  }
+
   return (
-    // <div className="center">
+    <React.Fragment>
       <div className="sidebar">
           <div className="logo">
-            <GiHamburgerMenu  id= "btn" />
+            <GiHamburgerMenu  id= "btn" onClick = {buttonClick}/>
           </div>
           <ul>
           <li>
             <a href="#">
             <i>
-              <BsInfoCircle  size = "1.4rem"/>
+              <BsInfoCircle  size = "1.4rem" className ="icons" />
               </i>
               <span className="font-link">About</span>
             </a>
+            <span className = "tooltip">About</span>
           </li>
           <li>
             <a href="#"><i>
-              <BsInstagram size = "1.4rem" />
+              <BsInstagram size = "1.4rem" className ="icons" />
               </i>
               <span className="font-link">Instagram</span>
             </a>
+            <span className = "tooltip">Instagram</span>
           </li>
           <li>
             <a href="#">
             <i>
-              <RiFacebookCircleLine size = "1.7rem"/>
+              <RiFacebookCircleLine size = "1.7rem" className ="icons" />
               </i>
               <span className="font-link">Facebook</span>
             </a>
+            <span className = "tooltip">Facebook</span>
           </li>
           <li>
             <a href="#">
               <i>
-              <IoGridOutline size = "1.4rem" />
+              <IoGridOutline size = "1.4rem" className ="icons"  />
               </i>
               <span className="font-link">Works</span>
             </a>
+            <span className = "tooltip">Works</span>
           </li>
           <li>
             <a href="#">
@@ -57,9 +69,15 @@ const SideBar = (props) => {
               {/* <div>hi</div> */}
               <span className="font-link" >Github</span>
             </a>
+            <span className = "tooltip">Github</span>
           </li>
           </ul>
       </div>
+      <div className ="home_content">
+      <div className = "text">Home</div>
+      </div>
+      </React.Fragment>
+      
     // </div>
   );
 };
